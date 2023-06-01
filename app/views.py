420522041d7043162fe287bf84d09333dfb46507
harshadvali11 <harshadvali1431@gmail.com>
2023-06-01 13:22:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def gowtham(request):
+    return render(request,'gowtham.html')
+
+def srujana(request):
+    return render(request,'srujana.html')
